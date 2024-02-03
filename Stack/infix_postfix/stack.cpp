faabcc58b5408ca39c10d6f4d15d64771ca7d3a1
TAUSEEF-01 <thesupremeemperor01@gmail.com>
2024-02-03 19:08:02 +0600

    updated

--- Stack/infix_postfix/stack.cpp
+++ Stack/infix_postfix/stack.cpp
@@ -1,24 +1,59 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-struct STACK
+// implementing stack
+struct Stack
 {
-    struct Stack
+    struct LinkedList
     {
         int val;
-        Stack *next;
+        char ch;
+        string str;
+        LinkedList* next;
     };
 
-    Stack *head = NULL;
-    Stack *tail = NULL;
+    LinkedList* head = NULL;
+    LinkedList* tail = NULL;
 
-    void push(int ch)
+    void push(char c)
     {
-        Stack *node = (Stack *)malloc(sizeof(Stack));
-        node->val = ch;
+        LinkedList* node = (LinkedList*)malloc(sizeof(LinkedList));
+        node->ch = c;
         node->next = NULL;
 
-        if (head == NULL)
+        if(head == NULL)
+        {
+            head = tail = node;
+            return;
+        }
+
+        tail->next = node;
+        tail = node;
+    }
+
+    void push(int v)
+    {
+        LinkedList* node = (LinkedList*)malloc(sizeof(LinkedList));
+        node->val = v;
+        node->next = NULL;
+
+        if(head == NULL)
+        {
+            head = tail = node;
+            return;
+        }
+
+        tail->next = node;
+        tail = node;
+    }
+
+    void push(string s)
+    {
+        LinkedList* node = (LinkedList*)malloc(sizeof(LinkedList));
+        node->str = s;
+        node->next = NULL;
+
+        if(head == NULL)
         {
             head = tail = node;
             return;
@@ -30,37 +65,125 @@ struct STACK
 
     void pop()
     {
-        if (head->next == NULL)
-        {
+        if(head == NULL)
             return;
-        }
 
-        Stack *node = head;
-        while (node->next != tail)
-        {
+        LinkedList* node = head;
+        while(node->next != tail)
             node = node->next;
-        }
+        
         tail = node;
         tail->next = NULL;
     }
 
-    int top()
+    char top(char c = 'c')
+    {
+        return tail->ch;
+    }   
+
+    int top(int v = 1)
     {
         return tail->val;
     }
 
+    string top(string s = "")
+    {
+        return tail->str;
+    }
+
     bool empty()
     {
-        if (head->next == NULL)
+        if(head == NULL)
             return true;
-        else
+        else 
             return false;
     }
 };
 
+
+
 int main()
 {
-    STACK stk;
+    Stack stk;
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// struct STACK
+// {
+//     struct Stack
+//     {
+//         int val;
+//         Stack *next;
+//     };
+
+//     Stack *head = NULL;
+//     Stack *tail = NULL;
+
+//     void push(int ch)
+//     {
+//         Stack *node = (Stack *)malloc(sizeof(Stack));
+//         node->val = ch;
+//         node->next = NULL;
+
+//         if (head == NULL)
+//         {
+//             head = tail = node;
+//             return;
+//         }
+
+//         tail->next = node;
+//         tail = node;
+//     }
+
+//     void pop()
+//     {
+//         if (head->next == NULL)
+//         {
+//             return;
+//         }
+
+//         Stack *node = head;
+//         while (node->next != tail)
+//         {
+//             node = node->next;
+//         }
+//         tail = node;
+//         tail->next = NULL;
+//     }
+
+//     int top()
+//     {
+//         return tail->val;
+//     }
+
+//     bool empty()
+//     {
+//         if (head->next == NULL)
+//             return true;
+//         else
+//             return false;
+//     }
+// };
+
+// int main()
+// {
+//     STACK stk;
+
+//     return 0;
+// }
